ace06740508a3c3b5e1365358bdbd7bcf4690dc8
Shankar Balakrishna <shankarnarayanb@gmail.com>
2020-02-22 21:44:32 +0000

    Added the necessary html, css and JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+import { lorem } from 'faker';
+
+export declare let require: any;
+const movieQuote  = require('popular-movie-quotes');
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-game';
+
+  randomText = movieQuote.getRandomQuote();
+  enteredText = '';
+  // solved = false;
+
+
+
+  onChangeLength(value: string) {
+    this.enteredText = value;
+    // if (value == this.randomText){
+    //   this.solved = true;
+    // }
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if(!enteredLetter){
+      return 'pending'
+    }
+    if (enteredLetter == randomLetter) {
+      return 'correct'
+    }
+    if (enteredLetter != randomLetter){
+      return 'incorrect'
+    }
+
+  }
+
 }
